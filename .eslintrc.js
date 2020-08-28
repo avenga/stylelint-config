7683808c17d60cc3c2a13a79064c9050d6176dfb
Fabian Mebus <hello@fabianmebus.com>
2020-08-28 18:22:20 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   env: {
+    commonjs: true,
+    es6: true,
     node: true
   },
   extends: '@avenga/eslint-config/javascript',
+  parserOptions: {
+    ecmaVersion: 2018
+  },
 };
